dd8c334b4edb258d2baee7d65082508a60ce2f25
Esa Korhonen <esa.korhonen@mariadb.com>
2024-11-27 12:15:31 +0200

    MXS-5366 Add test case
    Also, in the test, ensure that used private key can log in to
    all required backends.

--- system-test/mariadbmonitor/mariadbmon_rebuild_server.cc
+++ system-test/mariadbmonitor/mariadbmon_rebuild_server.cc
@@ -38,14 +38,47 @@ void test_main(TestConnections& test)
 
     auto& mxs = *test.maxscale;
     auto& repl = *test.repl;
-
-    // Copy ssh keyfile to maxscale VM.
+    auto* source_be = repl.backend(source_ind);
+    auto* target_be = repl.backend(target_ind);
+    // Copy ssh keyfile to maxscale VM from server1.
     const string keypath = "/tmp/sshkey.pem";
     mxs.vm_node().delete_from_node(keypath);
-    mxs.copy_to_node(repl.backend(0)->vm_node().sshkey(), keypath.c_str());
+    mxt::Node& key_source = repl.backend(0)->vm_node();
+
+    mxs.copy_to_node(key_source.sshkey(), keypath.c_str());
     auto chmod = mxb::string_printf("chmod a+rx %s", keypath.c_str());
     mxs.vm_node().run_cmd(chmod);
-    auto* target_be = repl.backend(target_ind);
+    // Read the contents of authorized_keys on server1. Check that the same line exists on server2 & 4.
+    // If not, edit the other files.
+    const string authorized_keys_path = mxb::string_printf("%s/.ssh/authorized_keys",
+                                                           key_source.access_homedir());
+    const string read_pubkey_cmd = mxb::string_printf("head -n1 %s", authorized_keys_path.c_str());
+    auto pubkey_res = key_source.run_cmd_output(read_pubkey_cmd);
+
+    if (pubkey_res.rc == 0 && !pubkey_res.output.empty())
+    {
+        test.tprintf("Expecting authorized_keys to contain line '%s'.", pubkey_res.output.c_str());
+        string grep_cmd = mxb::string_printf("cat %s | grep \"%s\"", authorized_keys_path.c_str(),
+                                             pubkey_res.output.c_str());
+        string concat_cmd = mxb::string_printf("echo \"%s\" >> %s", pubkey_res.output.c_str(),
+                                               authorized_keys_path.c_str());
+        for (auto* be : {source_be, target_be})
+        {
+            auto grep_res = be->vm_node().run_cmd_output(grep_cmd);
+            if (grep_res.rc != 0)
+            {
+                test.tprintf("Public key not found on %s, adding it.", be->vm_node().name());
+                be->vm_node().run_cmd_output(concat_cmd);
+                grep_res = be->vm_node().run_cmd_output(grep_cmd);
+                test.expect(grep_res.rc == 0, "Failed to add public key to %s.", be->vm_node().name());
+            }
+        }
+    }
+    else
+    {
+        test.add_failure("Command '%s' failed or gave no results. Error: %s",
+                         read_pubkey_cmd.c_str(), pubkey_res.output.c_str());
+    }
 
     mxs.start();
     mxs.check_print_servers_status(mxt::ServersInfo::default_repl_states());
@@ -63,7 +96,7 @@ void test_main(TestConnections& test)
         install_tools(target_ind);
 
         // Firewall on the source server may interfere with the transfer, stop it.
-        repl.backend(source_ind)->vm_node().run_cmd_output_sudo("systemctl stop iptables");
+        source_be->vm_node().run_cmd_output_sudo("systemctl stop iptables");
     }
 
     if (test.ok())
@@ -139,68 +172,106 @@ void test_main(TestConnections& test)
             test.expect(master_gtid_parts[1] != target_gtid_parts[1], "Gtid server_ids should be different");
             if (test.ok())
             {
-                auto res = mxs.maxctrl("call command mariadbmon async-rebuild-server MariaDB-Monitor "
-                                       "server4 server2");
-                if (res.rc == 0)
-                {
-                    // The op is async, so wait.
-                    bool op_success = false;
-                    mxb::StopWatch timer;
-                    while (timer.split() < 30s)
+                auto run_rebuild = [&test, &mxs, &repl]() {
+                    auto res = mxs.maxctrl("call command mariadbmon async-rebuild-server MariaDB-Monitor "
+                                           "server4 server2");
+                    if (res.rc == 0)
                     {
-                        auto op_status = mxs.maxctrl("call command mariadbmon fetch-cmd-result "
-                                                     "MariaDB-Monitor");
-                        if (op_status.rc != 0)
+                        // The op is async, so wait.
+                        bool op_success = false;
+                        mxb::StopWatch timer;
+                        while (timer.split() < 30s)
                         {
-                            test.add_failure("Failed to check rebuild status: %s", op_status.output.c_str());
-                            break;
-                        }
-                        else
-                        {
-                            auto& out = op_status.output;
-                            if (out.find("successfully") != string::npos)
+                            auto op_status = mxs.maxctrl("call command mariadbmon fetch-cmd-result "
+                                                         "MariaDB-Monitor");
+                            if (op_status.rc != 0)
                             {
-                                op_success = true;
+                                test.add_failure("Failed to check rebuild status: %s",
+                                                 op_status.output.c_str());
                                 break;
-                            }
-                            else if (out.find("pending") != string::npos
-                                     || out.find("running") != string::npos)
-                            {
-                                // ok, in progress
                             }
                             else
                             {
-                                // Either "failed" or something unexpected.
-                                break;
+                                auto& out = op_status.output;
+                                if (out.find("successfully") != string::npos)
+                                {
+                                    op_success = true;
+                                    break;
+                                }
+                                else if (out.find("pending") != string::npos
+                                         || out.find("running") != string::npos)
+                                {
+                                    // ok, in progress
+                                }
+                                else
+                                {
+                                    // Either "failed" or something unexpected.
+                                    break;
+                                }
                             }
+                            sleep(1);
                         }
-                        sleep(1);
+
+                        test.expect(op_success, "Rebuild operation failed.");
+
+                        if (test.ok())
+                        {
+                            // server4 should now be a slave and have same gtid as master.
+                            repl.sync_slaves();
+                            auto server_info = mxs.get_servers();
+                            server_info.print();
+                            mxs.wait_for_monitor();
+                            server_info.check_servers_status(mxt::ServersInfo::default_repl_states());
+                            auto master_gtid = server_info.get(0).gtid;
+                            auto target_gtid = server_info.get(target_ind).gtid;
+                            test.expect(master_gtid == target_gtid, "Gtids should be equal");
+                        }
                     }
-
-                    test.expect(op_success, "Rebuild operation failed.");
-
-                    if (test.ok())
+                    else
                     {
-                        // server4 should now be a slave and have same gtid as master.
-                        repl.sync_slaves();
-                        server_info = mxs.get_servers();
-                        server_info.print();
-                        mxs.wait_for_monitor();
-                        server_info.check_servers_status(mxt::ServersInfo::default_repl_states());
-                        master_gtid = server_info.get(0).gtid;
-                        target_gtid = server_info.get(target_ind).gtid;
-                        test.expect(master_gtid == target_gtid, "Gtids should be equal");
+                        test.add_failure("Failed to start rebuild: %s", res.output.c_str());
                     }
-                }
-                else
+                };
+                run_rebuild();
+
+                if (test.ok())
                 {
-                    test.add_failure("Failed to start rebuild: %s", res.output.c_str());
+                    // MXS-5366 Test username/password with special characters. This still does not test
+                    // a single quote ('), but perhaps that is rare enough to ignore for now. Supporting '
+                    // would require some extra string processing.
+                    auto change_monitor_user = [&](const string& user, const string& pw) {
+                        string cmd = mxb::string_printf("maxctrl alter monitor MariaDB-Monitor "
+                                                        "user='%s' password='%s'",
+                                                        user.c_str(), pw.c_str());
+                        auto rc = mxs.vm_node().run_cmd(cmd);
+                        test.expect(rc == 0, "Alter monitor command '%s' failed.", cmd.c_str());
+                        mxs.check_print_servers_status(mxt::ServersInfo::default_repl_states());
+                    };
+
+                    string tricky_user_str = "\"#¤%&/\\()=?";
+                    // The backslashes have to be doubled for mariadb client.
+                    string tricky_user_client = "\"#¤%&/\\\\()=?";
+                    string tricky_user_pw = "åÄÖ*,.-_";
+                    auto user = repl.backend(0)->admin_connection()->create_user(
+                        tricky_user_client, "%", tricky_user_pw);
+                    user.grant("all privileges on *.*");
+                    test.tprintf("User '%s' created. Testing monitor and rebuild-server with it.",
+                                 tricky_user_str.c_str());
+                    repl.sync_slaves();
+                    change_monitor_user(tricky_user_str, tricky_user_pw);
+
+                    target_be->admin_connection()->cmd("stop slave;");
+                    mxs.wait_for_monitor();
+                    run_rebuild();
+
+                    test.tprintf("Resetting monitor user and password.");
+                    change_monitor_user("mariadbmon", "mariadbmon");
                 }
             }
         }
         rwsplit_conn->cmd("drop database test;");
     }
 
-    repl.backend(source_ind)->vm_node().run_cmd_output_sudo("systemctl start iptables");
+    source_be->vm_node().run_cmd_output_sudo("systemctl start iptables");
     mxs.vm_node().delete_from_node(keypath);
 }
